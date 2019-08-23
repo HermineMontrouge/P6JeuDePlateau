f5871bd700068d0c661cc496c1e04ac1875769e5
Hermine Montrouge <herminemontrouge@gmail.com>
2019-08-23 09:46:00 +0200

    Add ID for each box

--- board.js
+++ board.js
@@ -24,6 +24,15 @@ class Board {
     }
   }
 
+  generateId() {
+    const boxes = document.querySelectorAll(".box");
+    let index = 0;
+
+    for (let eachBox of boxes) {
+      eachBox.id = index++;
+    }
+  }
+
   generateUnreachableBoxes() {
     // Selection of class="box" on the dom
     const boxes = document.querySelectorAll(".box");
@@ -35,7 +44,7 @@ class Board {
 
     for (let i = 0; i < numberOfUnreachableBoxes; i++) {
       const unreachableBox = boxes[Math.floor(Math.random() * boxes.length)];
-      unreachableBox.className = " box unreachable";
+      unreachableBox.className = `box unreachable`;
       // console.log(unreachableBox);
       // renvoie <div class=" box unreachable"></div>
     }
@@ -47,15 +56,17 @@ class Board {
 
   genaratePlayers() {
     const boxes = document.querySelectorAll(".box");
+    const player = document.querySelectorAll(".player");
     // generate one player of each in random boxes
     for (let i = 0; i < 2; i++) {
-      const newPlayer = boxes[Math.floor(Math.random() * boxes.length)];
+      this._players = boxes[Math.floor(Math.random() * boxes.length)];
       if (i === 0) {
-        newPlayer.className = `box player1 y-${this._row} x-${this._box}`;
+        this._players.className = `box player1`;
       } else {
-        newPlayer.className = `box player2 y-${this._row} x-${this._box}`;
+        this._players.className = `box player2`;
       }
-      console.log(newPlayer);
+      console.log(player);
+      // console.log(players[1]);
       console.log(boxes[1]);
     }
     console.log(boxes[4]);
@@ -64,17 +75,17 @@ class Board {
   generateWeapons() {
     const boxes = document.querySelectorAll(".box");
     for (let i = 0; i < 4; i++) {
-      const newWeapon = boxes[Math.floor(Math.random() * boxes.length)];
+      this._weapons = boxes[Math.floor(Math.random() * boxes.length)];
       if (i === 0) {
-        newWeapon.className = `box weapon1`;
+        this._weapons.className = `box weapon1`;
       } else if (i === 1) {
-        newWeapon.className = `box weapon2`;
+        this._weapons.className = `box weapon2`;
       } else if (i === 2) {
-        newWeapon.className = `box weapon3`;
+        this._weapons.className = `box weapon3`;
       } else if (i === 3) {
-        newWeapon.className = `box weapon4`;
+        this._weapons.className = `box weapon4`;
       }
-      console.log(newWeapon);
+      console.log(this._weapons);
     }
   }
 }
@@ -84,6 +95,94 @@ class Board {
 const newBoard = new Board(4, 4, 2, 2, 4);
 
 newBoard.generateBoard();
+newBoard.generateId();
 newBoard.generateUnreachableBoxes();
 newBoard.genaratePlayers();
 newBoard.generateWeapons();
+
+// newBoard.noSideBySide();
+
+// noSideBySide() {
+//   while (this.players[1] == this.players || this.players[1] == (this.players + 1) ||
+//     this.players[1] == (this.players - 1) || this.players[1] == (this.players + 10) ||
+//     this.players[1] == (this.players - 10) || this.players[1] == (this.players + 9) ||
+//     this.players[1] == (this.players - 9) || this.players[1] == (this.players + 11) ||
+//     this.players[1] == (this.players - 11)) {
+//     this.players[1] = Math.floor(Math.random() * this.boxes.length);
+//   }
+//   console.log(this.players[1]);
+// }
+
+// playerProximity() {
+//   /**
+//    * Détection de la proximité des joueurs
+//    */
+//       var sideBySide = false;
+//       for (let x = 0; x < this.gameMap.board.length; x++) {
+//           for (let y = 0; y < this.gameMap.board.length; y++) {
+//               if (this.gameMap.board[x][y].player == this.currentPlayer) {
+//                   if (x < 9 && this.gameMap.board[x + 1][y].player == this.currentEnemy){
+//                       sideBySide = true;
+//                   }
+//                   else if (x > 0 && currentGame.gameMap.board[x - 1][y].player == currentGame.currentEnemy){
+//                       sideBySide = true;
+//                   }
+//                   else if (y < 9 && currentGame.gameMap.board[x][y + 1].player == currentGame.currentEnemy){
+//                       sideBySide = true;
+//                   }
+//                   else if (y > 0 && currentGame.gameMap.board[x][y - 1].player == currentGame.currentEnemy){
+//                       sideBySide = true;
+//                   }
+//               }
+//           }
+//       }
+//       return sideBySide;
+//   }
+
+// recupererUneCaseBlanche: function() // Choisis au hasard une case blanche, pour mettre en paramètre de la fonction compterCasesAccessibles
+// {
+// 	let caseBlanche = "";
+// 	while(caseBlanche === "")
+// 	{
+// 		let ligneAleatoire = Math.floor(Math.random()*Plateau.nbLignes); // On pioche aléatoirement une ligne et une colonne pour créer les coordonnées d'une case
+// 		let colonneAleatoire = Math.floor(Math.random()*Plateau.nbColonnes);
+// 		if (Plateau.grille[ligneAleatoire][colonneAleatoire].estVide())
+// 		{
+// 			caseBlanche = Plateau.grille[ligneAleatoire][colonneAleatoire];
+// 		}
+// 		else
+// 		{ // On recommence
+// 		}
+// 	}
+// 	return caseBlanche;
+// }
+
+// getEmptyCells() {
+//   /**
+//    * Récupération des cases vides
+//    */
+//       let emptyCells = new Array();
+//       for (let x = 0; x < this.mapSize; x++) {
+//           for (let y = 0; y < this.mapSize; y++) {
+//               if (this.board[x][y].barrel == false) {emptyCells.push({ X: x, Y: y });}
+//           }
+//       }
+//       return emptyCells;
+//   }
+
+// lightAccessibleCells() {
+//   /**
+//    * Surlignage des cases acccessibles au joueur
+//    */
+//       let myBoard = currentGame.gameMap.board;
+//       for (let x = 0; x < currentGame.gameMap.board.length; x++) {
+//           for (let y = 0; y < currentGame.gameMap.board.length; y++) {
+//               if (myBoard[x][y].highlight == true && myBoard[x][y].barrel == false && myBoard[x][y].player == null) {
+//                   $('#' + x + y).addClass("light");
+//               }
+//               else {
+//                   this.board[x][y].highlight = false;
+//               }
+//           }
+//       }
+//   }
